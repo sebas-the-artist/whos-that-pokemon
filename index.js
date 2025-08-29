e54020f1053c394f193b57c2d39172d004b89033
sebastian <manijustworkhere@gmail.com>
2025-08-29 04:25:40 -0400

    got the search bar working now for... litterally everything else

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-fetchData()
 
 async function fetchData() {
 
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
     if(!response.ok) {
         console.log("thats not gonna work big dog")
     }
     else{
-        const data = await response.json()
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite
+        imgElement.style.display = "block"
         console.log(data)
     }
 }
