330e03f48a641c35328411da555cae77d05be043
sebastian <manijustworkhere@gmail.com>
2026-01-11 23:40:15 -0500

    c-137 pps

---
+++ ketamine.jsx
@@ -0,0 +1,20 @@
+class Car {
+  constructor(brand, year) {
+    this.brand = brand;
+    this.year = year;
+  }
+  getAge() {
+    let date = new Date();
+    return date.getFullYear() - this.year;
+  }
+  print() {
+    console.log(
+      ` This Fuckin ${this.year} ${this.brand} Is ${this.getAge()} years old `
+    );
+  }
+}
+const racecar = new Car("nissan", "1992");
+const cwassy = new Car("lincoln", "1981");
+
+racecar.print();
+cwassy.print();
