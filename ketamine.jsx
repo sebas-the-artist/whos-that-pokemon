9ddae14c07cf7cc030733b6a64d99779a93a9fa3
sebastian <manijustworkhere@gmail.com>
2026-02-05 00:00:07 -0500

    sjd

--- ketamine.jsx
+++ ketamine.jsx
@@ -14,7 +14,7 @@ class Car {
   }
 }
 const racecar = new Car("Honda", "1992");
-const cwassy = new Car("Lincoln", "1981");
+const cwassy = new Car("Lincoln", "1980");
 
 racecar.print();
 cwassy.print();
