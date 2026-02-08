3fb0bea28e8bcce47cea913c7d21bf6a8a1a4f3b
sebastian <manijustworkhere@gmail.com>
2026-02-08 00:20:47 -0500

    only 2 people know the kayrahtay. the chinese, and the king

--- ketamine.jsx
+++ ketamine.jsx
@@ -9,12 +9,12 @@ class Car {
   }
   print() {
     console.log(
-      ` This Fuckin ${this.year} ${this.brand} Is ${this.getAge()} years old `
+      ` This Fuckin ${this.year} ${this.brand} Is ${this.getAge()} years old `,
     );
   }
 }
 const racecar = new Car("Honda", "1992");
-const cwassy = new Car("Lincoln", "1980");
+const cwassy = new Car("Lincoln", "1977");
 
 racecar.print();
 cwassy.print();
