22599351e281d237a5b1a6acb8b1871796017a4e
Sebastian Martinez <manijustworkhere@gmail.com>
2026-04-04 23:44:30 -0400

    lldj-jp

--- ketamine.jsx
+++ ketamine.jsx
@@ -14,7 +14,7 @@ class Car {
   }
 }
 const racecar = new Car("Honda", "1992");
-const cwassy = new Car("Lincoln", "1983");
+const cwassy = new Car("Lincoln", "1982");
 
 racecar.print();
 cwassy.print();
