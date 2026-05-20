e3573679f31e83271e7627bb0bbfc408b45744e7
Sebastian Martinez <manijustworkhere@gmail.com>
2026-05-19 22:28:49 -0400

    jjdjjdjjdjjdjdjdmy gorl got sushi

--- ketamine.jsx
+++ ketamine.jsx
@@ -14,7 +14,7 @@ class Car {
   }
 }
 const racecar = new Car("Honda", "1985");
-const cwassy = new Car("Lincoln", "1960");
+const cwassy = new Car("Lincoln", "1963");
 
 racecar.print();
 cwassy.print();
