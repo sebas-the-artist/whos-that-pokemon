8054365328cb5b9970cf64a8cdd660a03403eb8f
Sebastian Martinez <manijustworkhere@gmail.com>
2026-07-13 16:14:55 -0400

    iiiiiiiiiiiiittttsss sjohn cennaaaaaaaa

--- ketamine.jsx
+++ ketamine.jsx
@@ -14,7 +14,7 @@ class Car {
   }
 }
 const racecar = new Car("ford", "1985");
-const cwassy = new Car("Lincoln", "1978");
+const cwassy = new Car("Lincoln", "1974");
 
 racecar.print();
 cwassy.print();
